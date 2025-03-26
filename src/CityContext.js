c88cac315977f0ec50e2d3f3781d16a4a50b829d
elsanaa1 <sanaa.jerbouh1@gmail.com>
2025-03-26 01:56:40 +0000

    city context provider reducer

--- src/CityContext.js
+++ src/CityContext.js
@@ -1,5 +1,36 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
-// context creation
+// context creation: this context will hold data about city 
 export const CityContext=createContext();
+
+// creation of reducer 
+const Reducer=(state,action)=>{
+     switch(action.type){
+        case 'FILTERCITY': return action.payload;// filter city when user enter a city name iin filter input 
+        default: return state;
+     }
+}
+
+// init value state 
+const initState={
+    city:'Casablanca'
+}
+
+// provider creation
+export const WeatherProvider=({children})=>{
+
+     // **reducer link to my provider
+    const [state,dispatch]=useReducer(Reducer,initState);
+
+    // return provider
+    return (
+        <CityContext.Provider value={{state,dispatch}}>
+            {children}
+        </CityContext.Provider>
+    )
+
+}
+
+
+
 
